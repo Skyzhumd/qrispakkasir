01fcc2a16b3c344fe3e0b9b91ceffa79ced3c1ab
Skyzhumd <141169997+Skyzhumd@users.noreply.github.com>
2026-03-11 06:29:13 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,6 +1,6 @@
 module.exports = {
     botToken: "8623129994:AAFz5f4dfTD1H-xf0ZGeD-EgNnFg_lwxjYE",
-    pakasirApiKey: "API_KEY_PAKASIR",
+    pakasirApiKey: "zj5awjF1251jfqJQsyapHU3qRYRNuX8t",
     pakasirSlug: "hosting-pterodactyl",
     adminId: "zj5awjF1251jfqJQsyapHU3qRYRNuX8t",
     defaultAmount: 10000
