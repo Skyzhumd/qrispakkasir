a45143c45f6f26218bc8998e37ae204a8a014747
Skyzhumd <141169997+Skyzhumd@users.noreply.github.com>
2026-03-11 06:55:14 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,7 +1,7 @@
 module.exports = {
     botToken: "8623129994:AAFz5f4dfTD1H-xf0ZGeD-EgNnFg_lwxjYE",
     pakasirApiKey: "zj5awjF1251jfqJQsyapHU3qRYRNuX8t",
-    pakasirSlug: "hosting-pterodactyl",
+    pakasirSlug:'hosting-pterodactyl',
     adminId: "8521019587",
     defaultAmount: 100
 };
