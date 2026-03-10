e8f0acc5edffd6d6036e296d4b15c076a2c5db2e
Skyzhumd <141169997+Skyzhumd@users.noreply.github.com>
2026-03-11 06:23:46 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botToken: "8623129994:AAFz5f4dfTD1H-xf0ZGeD-EgNnFg_lwxjYE",
+    pakasirApiKey: "API_KEY_PAKASIR",
+    pakasirSlug: "hosting-pterodactyl",
+    adminId: "zj5awjF1251jfqJQsyapHU3qRYRNuX8t",
+    defaultAmount: 10000
+};
